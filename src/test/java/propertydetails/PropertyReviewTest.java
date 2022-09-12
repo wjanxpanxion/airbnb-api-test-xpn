52f692f4dc0a53267be30821223397764c3fa818
wjanxpanxion <wjanxpanxion@gmail.com>
2022-09-12 10:27:22 -0400

    delete space

--- src/test/java/propertydetails/PropertyReviewTest.java
+++ src/test/java/propertydetails/PropertyReviewTest.java
@@ -16,18 +16,6 @@ import java.util.List;
 
 public class PropertyReviewTest {
 
-    /*
-    Steps:
-    1- Move the common code to base test and add the @BeforeMethod
-    2- Move the core logic to Api Client class with the reusable method
-    3- Now in the Test method use the Api client object and call the respective reusable method passing POJO object as an argument.
-
-
-    H.W:
-    1- Complete the manual test case spread sheet.
-    2- Move all the core logic to respective Api Client classes and repeat the no.3 above.
-     */
-
     String propertyID1083329 = "1083329";
     String getPropertyIDEmpty= "";
 
